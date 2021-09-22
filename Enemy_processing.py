824d255ba870e6a14f89dd3713813de1a7176833
Apolliner de Gray <plavec94@yandex.ru>
2021-09-22 22:29:35 +0300

    Реализация гобального А* на чистых Global_vertices

--- Enemy_processing.py
+++ Enemy_processing.py
@@ -1227,4 +1227,131 @@ class path_calculate:
             check_node = graph[check_node.direction] #Предыдущая вершина объявляется проверяемой
 
         return list(reversed(reversed_waypoints)), success
+
+
+    def _world_vertices_a_star_algorithm(vertices_map, start_point, finish_point, global_or_local, enemy):
+        """
+            Рассчитывает поиск пути, алгоритмом A* с использованием исключительно полей доступности.
+
+            Вообще, если бы не желание запихнуть в одну функцию обработку глобальных и локальных координат,
+            то надо было сразу перевести глобальный рассчёт на чистые Global_vertices,
+            ведь они уже и так содержат всю необходимую информацию.
+
+            ЗАДАЧИ:
+            Возможно, можно порезать код на отдельные функции, что бы повторно использовать код.
+
+            ОСОБЕННОСТИ:
+            Есть путаница с number глобальных Global_vertices и локальных Node_vertices
+
+            Сюда приходит:
+            Список вершин - vertices_map;
+            Cтартовая вершина - start_vertices - class Global_vertices;
+            Финишная вершина - finish_vertices - class Global_vertices;
+
+            return список вершин - вейпоинтов
+            
+        """
+        class Node_vertices:
+            """Содержит узлы графа для работы с зонами доступности"""
+            __slots__ = ('number', 'vertices', 'position', 'price', 'direction', 'ready')
+            def __init__(self, number, vertices, position, price, direction):
+                self.number = number
+                self.vertices = vertices
+                self.position = position
+                self.price = price
+                self.direction = direction #Хранит номер вершины из которой вышла
+                self.ready = True #Проверена ли точка
+
+        def path_length(start_point, finish_point):
+            """
+                Вычисляет примерное расстояния до финиша, для рассчётов стоимости перемещения
+            """
+            return math.sqrt((start_point[0] - finish_point[0])**2 + (start_point[1] - finish_point[1])**2)
+
+        def node_connections(vertices_map, graph, processed_node, finish_vertices, verified_vertices):
+            """
+                Определяет связи вершины и добавляет их в граф при расчёте по глобальной карте
+            """
+            processed_node.ready = False
+            #Находим указанную зону доступности
+            for vertices in vertices_map:
+                if vertices.number == processed_node.vertices:
+                    #Проверяем, есть ли у неё связи
+                    if vertices.connections:
+                        for connect in vertices.connections:
+                            if not connect.number in verified_vertices:
+                                verified_vertices.append(connect.number)
+                                graph.append(Node_vertices(len(graph), connect.number, connect.position, path_length(connect.position,
+                                                                                        finish_vertices.position), processed_node.number))
+
+        
+        graph = [] #Список, содержащий все вершины
+        verified_position = [] #Содержит список всех использованных координат, что бы сравнивать с ним при добавлении новой вершины.
+        graph.append(Node_vertices(0, start_vertices.number, start_vertices.position,
+                                    path_length(start_vertices.position, finish_vertices.position), 0))
+        verified_vertices.append(start_vertices.number)
+
+        node_connections(vertices_map, graph, graph[0], finish_vertices, verified_vertices)
+
+        general_loop = True #Параметр останавливающий цикл
+        step_count = 0 #Шаг цикла
+        number_finish_node = 0 #Хранит номер финишной точки
+        reversed_waypoints = [] #Обращенный список вейпоинтов
+        success = True #Передаёт информацию об успехе и не успехе
+        while general_loop:
+            min_price = 99999
+            node = graph[-1]
+            for number_node in range(len(graph)):
+                if graph[number_node].ready:
+                    if graph[number_node].price < min_price:
+                        min_price = graph[number_node].price
+                        node = graph[number_node]
+            if min_price == 99999:
+                number_finish_node = len(graph) - 1
+                success = False
+                general_loop = False
+
+                enemy.target = []
+
+            node_connections(vertices_map, graph, node, finish_vertices, verified_vertices)
+
+            if node.vertices == finish_vertices.number:
+                number_finish_node = node.number
+                general_loop = False
+
+            step_count += 1
+            if step_count == 300:
+                last_check_success = False
+                for last_check in verified_vertices:
+                    if last_check == finish_vertices.number:
+                        for node in graph:
+                            if node.vertices == last_check:
+                                number_finish_node = node.number
+                                last_check_success = True
+                                general_loop = False
+                if not last_check_success:
+                    min_price = 99999
+                    node = graph[-1]
+                    for number_node in range(len(graph)):
+                        if graph[number_node].price < min_price:
+                            min_price = graph[number_node].price
+                            number_finish_node = number_node
+
+                    enemy.target = []
+                    success = False
+                    general_loop = False
+                
+        check_node = graph[number_finish_node]
+
+        ran_while = True
+        while ran_while:
+            reversed_waypoints.append(check_node.vertices)
+            if check_node.direction == 0:
+                ran_while = False
+            check_node = graph[check_node.direction] #Предыдущая вершина объявляется проверяемой
+
+        return list(reversed(reversed_waypoints)), success
+
+
+    
         
