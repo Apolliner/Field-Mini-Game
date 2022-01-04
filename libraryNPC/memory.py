f1446ebc5d8092ad8424bc8409cae472672adc0a
Apolliner <plavec94@yandex.ru>
2022-01-04 22:25:09 +0300

    Развитие логики памяти

--- libraryNPC/memory.py
+++ libraryNPC/memory.py
@@ -3,11 +3,14 @@
 class MemoryNode:
     """
         Вершина графа памяти
-        Каждое событие как протокол - должно быть закрыто. Закрытые события обобщаются для экономии места.
+        Каждое событие должно быть закрыто. Закрытые события обобщаются для экономии места.
     """
-    def __init__(self, number, type, connection=None):
+    def __init__(self, number, type, name, status, entity, connection=None):
         self.number = number
         self.type = type
+        self.name = name
+        self.status = status
+        self.entity = entity
         self.connections = list()
         if connection:
             self.connections.append(connection)
@@ -16,7 +19,7 @@ class MemoryNode:
 class Memory:
     """
         Содержит в себе граф памяти
-        Память хранит события и обобщенные события
+        Память хранит события, обобщенные события и их связи
 
         _nodes содержит элементы памяти
         _graph содержит связи между элементами памяти
@@ -25,3 +28,9 @@ class Memory:
         self._nodes = list()
         self._graph = dict()
         self._event_stack = list()
+
+    def add_standard_memories(self):
+        """ Заполняет память стандартными знаниями """
+        self._nodes.extend([
+                MemoryNode(len(self._nodes), "area", "base_area", "opened", None),  # Запись обо всём мире
+                MemoryNode(len(self._nodes + 1), "character", "target", "opened", "<Объект персонажа игрока>")])
