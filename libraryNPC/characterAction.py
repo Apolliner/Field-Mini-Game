0908302ed520c2f94f7cdc12580923d6b55110ed
Apolliner <plavec94@yandex.ru>
2022-02-01 22:09:35 +0300

    Продолжение работы над действием NPC

--- libraryNPC/characterAction.py
+++ libraryNPC/characterAction.py
@@ -27,6 +27,7 @@ class CharacterAction(Bases):
         locations_list = list()
         firewood_set = set(dry)
         global_map = kwargs["global_map"]
+        vertices_dict = kwargs["vertices_dict"]
         lines = global_map[self.global_position[0] - 1:self.global_position[0] + 1]
         for number_line, line in enumerate(lines):
             locations = line[self.global_position[1] - 1:self.global_position[1] + 1]
@@ -37,10 +38,32 @@ class CharacterAction(Bases):
                     locations_list.append([self.global_position[0] + number_line - 1],
                                           [self.global_position[1] + number_location - 1])
         # Список локаций вокруг персонажа, на которых есть дрова собран.
+        if not locations_list:
+            return True
+        for location_position in locations_list:
+            vertices_list = global_map[location_position[0], location_position[1]].vertices
+            check_vertices = None
+            for vertices in vertices_list:
+                # Проверка на возможность достичь точки
+                _, success = self._path_world_vertices_a_star_algorithm(kwargs["vertices_dict"], self.vertices,
+                                                                        vertices.number)
+                if success:
+                    check_vertices = vertices.number
+                    break
+            if check_vertices is not None:
+                target = self.memory.add_memories("target", "move", positions=[check_vertices], **kwargs)
+                self.action_stack.add_stack_element(name="local_move", element=self.path_move, target=target)
+                yield False
+                self_tile = self.bases_world_tile(global_map, self.world_position)
+                if self_tile.icon in dry:
+                    return True
+        return True
+
+                
 
 
     def _action_collect_firewood(self, **kwargs):
-        """ 3 раза ищет дрова или ветки. Собирает их и приносит в одну точку """
+        """ 3 раза ищет дрова или ветки. Собирает их и приносит в стартовую точку """
         ...
 
     def _action_arrange_a_fire_pit(self, **kwargs):
