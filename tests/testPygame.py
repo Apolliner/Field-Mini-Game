f3c09bb38b7c6681867877f8d9e5ee76d4e9c024
Apolliner <plavec94@yandex.ru>
2022-03-01 20:51:44 +0300

    Масштабирование тачскрином

--- tests/testPygame.py
+++ tests/testPygame.py
@@ -86,7 +86,7 @@ clock = pygame.time.Clock()
 grass = pygame.image.load(os.path.join(os.path.dirname(__file__), './resources', 'tile_grass_0.png')).convert()
 stones = pygame.image.load(os.path.join(os.path.dirname(__file__), './resources', 'tile_stone_0.png')).convert()
 bonfire = pygame.image.load(os.path.join(os.path.dirname(__file__), './resources', 'tile_bonfire_2.png')).convert_alpha()
-enemy = pygame.image.load(os.path.join(os.path.dirname(__file__), './resources', 'tile_enemy_riffleman_down_squat_1.png')).convert_alpha()
+enemy = pygame.image.load(os.path.join(os.path.dirname(__file__), './resources', 'tile_enemy_riffleman_down_0.png')).convert_alpha()
 
 
 class ColorTile(pygame.sprite.Sprite):
@@ -116,33 +116,22 @@ class Tile(pygame.sprite.Sprite):
         self.old_size_tile = size_tile
         self.number_x = number_x
         self.number_y = number_y
-        #self.color = color
-        #self.alpha = alpha
         pygame.sprite.Sprite.__init__(self)
         self.img = image_tile
         self.image = pygame.transform.scale(self.img, (size_tile, size_tile))
         self.rect = self.image.get_rect()
-        #self.image = pygame.Surface((size_tile, size_tile))
-        #self.image.fill(self.color)
-        #self.image.set_alpha(self.alpha)
-        #self.rect = self.image.get_rect()
         self.rect.left = zero_x + number_x*size_tile
         self.rect.top = zero_y + number_y*size_tile
         self.speed = 0
 
     def draw(self, surface):
-        pass
-        #if self.rect.top > 150 and self.rect.left > 150:
-        #    surface.blit(self.image, self.rect)
+        surface.blit(self.image, self.rect)
 
     def update(self, zero_x, zero_y, size_tile, up=False):
         if up:
             if self.number_y == 0:
                 self.kill()
             self.number_y -= 1
-        #self.image = pygame.Surface((size_tile, size_tile))
-        #self.image.fill(self.color)
-        #self.image.set_alpha(self.alpha)
         position_x = zero_x + self.number_x * size_tile
         position_y = zero_y + self.number_y * size_tile
         if self.old_size_tile != size_tile:
@@ -151,11 +140,6 @@ class Tile(pygame.sprite.Sprite):
         self.rect.left = position_x
         self.rect.top = position_y
         self.old_size_tile = size_tile
-        #else:
-            #print(F"position_x - {position_x}, position_y - {position_y}")
-            #self.rect = pygame.Surface((size_tile, size_tile)).fill(RED)#.get_rect()
-
-
 
 len_x, len_y = size = screen.get_width(), screen.get_height()
 center_x = len_x/2
@@ -174,18 +158,20 @@ for number_line, line in enumerate(fields):
         else:
             group.add(Tile(zero_x, zero_y, number_tile, number_line, size_tile, grass))
 
+person_tile = Tile(zero_x, zero_y, len(fields)//2, len(fields[0])//2, size_tile, enemy)
+#person_tile = Tile(zero_x, zero_y, 1, 1, size_tile, enemy)
 font = pygame.font.SysFont("Arial", 18)
-
 
 def update_fps():
     fps = str(int(clock.get_fps()))
     fps_text = font.render(fps, 1, pygame.Color("coral"))
     return fps_text
-
+print(dir(pygame))
 x = 100
 y = 200
 size_tile = 30
-
+person_x = 0
+person_y = 0
 x_plus = 0
 y_plus = 0
 # Цикл игры
@@ -196,11 +182,13 @@ color_alpha_tile = ColorTile(200, 300, 50, RED, 100)
 working_surface = pygame.Surface((800, 800))
 working_surface_position = (50, 50)
 while running:
+    pygame.key.set_repeat(1, 2)
     step += 1
     # Держим цикл на правильной скорости
     clock.tick(FPS)
     # Ввод процесса (события)
     for event in pygame.event.get():
+        print(event)
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
@@ -220,6 +208,17 @@ while running:
                 motion_x, motion_y = event.rel
                 x_plus += motion_x
                 y_plus += motion_y
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                person_x -= 0.01
+            if event.key == pygame.K_RIGHT:
+                person_x += 0.01
+            if event.key == pygame.K_UP:
+                person_y -= 0.01
+            if event.key == pygame.K_DOWN:
+                person_y += 0.01
+        if event.type == pygame.MULTIGESTURE:
+            size_tile += event.pinched * 100
     # Рендеринг
     screen.fill(BLUE)
     len_x, len_y = size = screen.get_width(), screen.get_height()
@@ -233,11 +232,13 @@ while running:
         for i in range(len_fields):
             group.add(Tile(zero_x, zero_y, i, len_fields - 1, size_tile, random.choice([stones, grass])))
         group.update(zero_x, zero_y, size_tile, up=True)
-
     else:
         group.update(zero_x, zero_y, size_tile)
     working_surface.fill(GREEN)
     group.draw(working_surface)
+
+    person_tile.update(zero_x + person_x * size_tile, zero_y + person_y * size_tile, size_tile)
+    person_tile.draw(working_surface)
     screen.blit(working_surface, working_surface_position)
     #color_alpha_tile.draw(screen)
     screen.blit(update_fps(), (10, 0))
