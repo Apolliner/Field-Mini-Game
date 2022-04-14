66f190b9382be9ce0de8a4126b5306292304d9e7
Apolliner <plavec94@yandex.ru>
2022-04-14 22:58:52 +0300

    Разработка логики работы с инвентарём

--- libraryNPC/characterInventory.py
+++ libraryNPC/characterInventory.py
@@ -1,3 +1,4 @@
+import random
 from libraryNPC.bases import Bases
 from libraryNPC.item import Item
 
@@ -5,7 +6,33 @@ class CharacterInventory(Bases):
     """ Инвентарь персонажа """
     _inventory = {"other": [], "meal": []}
 
-    def inventory_get_item(self, type, count=1):
+    def inventory_get_item(self, type, name=None, count=1):
+        """ Посмотреть указанные предмет из инвентаря """
         if type in self._inventory and self._inventory[type]:
             items = self._inventory[type]
-    
+            if name is None:
+                return random.choice(items)
+            else:
+                for item in items:
+                    if item.name == name:
+                        return item
+            return None
+
+    def inventory_add_item(self, item):
+        """ Добавить предмет в инвентарь """
+        if item.type in self._inventory:
+            self._inventory[item.type].append(item)
+        else:
+            self._inventory[item.type] = [item]
+
+    def inventory_extract_item(self, item):
+        """ Извлечь указанный предмет из инвентаря """
+        if item.type in self._inventory:
+            for number_check_item, check_item in enumerate(self._inventory[item.type]):
+                if check_item is item:
+                    self._inventory[item.type].pop(number_check_item)
+                    return True
+        return False
+        
+
+
